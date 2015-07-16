3b9264c67d364e81fdcb179743ee8bca5ef4d187
tommyputranto <tommyputranto1@gmail.com>
2015-07-16 15:04:31 +0700

    update4
    sudah sesuai nama dan foto

--- app/src/main/java/com/androbos/dosen/MainActivity.java
+++ app/src/main/java/com/androbos/dosen/MainActivity.java
@@ -4,13 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listview;
+    private CustomListViewAdapter customListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.row);
+        setContentView(R.layout.activity_main);
+        final String[] namaDosen = new String[]{
+                "Eko Setijadi, ST., MT., Ph.D","Ir. Hany Boedinugroho, MT."
+        };
+        final String[] nip = new String[]{
+                "197210012003121002","196107061987011001"
+        };
+        final Integer[] foto = new Integer[]{
+                R.drawable.ekoset,R.drawable.hani
+        };
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+        for (int i=0;i<namaDosen.length;i++){
+            HashMap<String, String> data = new HashMap<>();
+            data.put("namaDosen",namaDosen[i]);
+            data.put("nip",nip[i]);
+            list.add(data);
+        }
+        ArrayList<HashMap<String,Integer>> list1= new ArrayList<>();
+        for (int i=0; i <namaDosen.length;i++){
+            HashMap<String,Integer> data = new HashMap<>();
+            data.put("foto",foto[i]);
+            list1.add(data);
+        }
+        listview = (ListView)findViewById(R.id.list_item);
+        customListViewAdapter = new CustomListViewAdapter(getApplicationContext(),list,list1);
+        listview.setAdapter(customListViewAdapter);
     }
 
     @Override
@@ -35,3 +66,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
